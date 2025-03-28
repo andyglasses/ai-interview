9fc61e3105a6f7798ca776ec81af94b9ed7b145b
Andy Glass <andyglasses@gmail.com>
2025-03-28 09:18:18 +0000

    Add missing pre-kickoff stuff

---
+++ test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AI.Interview.Tests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
